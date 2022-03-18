7aa07c5d9387464a86deca2ccc1c0d36e2c6e26e
Alessio1921 <caruso.alessio1999@gmail.com>
2022-03-18 15:21:39 +0100

    exercise complete. bonus to be added

--- js/script.js
+++ js/script.js
@@ -7,12 +7,19 @@ const app=new Vue({
   methods:{
     addItem(userText){
       if(userText!==""){
-        this.list.push({text:userText.trim()}),
+        this.list.push({
+          text:userText.trim(),
+          done:false
+        }),
         this.userText=""
       }
     },
     removeItem(index){
       this.list.splice(index, 1)
+    },
+    checked(index){
+      this.list[index].done=!this.list[index].done,
+      console.log(this.list[index].done)
     }
   }
 })
